73770cd904fc24c479e099aa0b3c25e3aaee5305
rmit-s3484936-michal-matera <s3484936@student.rmit.edu.au>
2015-05-13 16:20:17 +1000

    Added an Id to every seat
    Id example :
    ssA01
    
    ss- standard seat
    fs-first class seat
    bb- bean bag
    
    A01-seat row and number

--- seats.php
+++ seats.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<link rel="stylesheet" href="css/style-seats.css">
+		<link rel="stylesheet" href="css/style.css">
 		<title>Seats</title>
 	</head>
 	<body>
@@ -9,155 +10,173 @@
 			<table id:"seats1">
 				<tr>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssH01"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssH02"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssH03"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssH04"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<div class="sSeat"></div>
-					</td>
-					<td>
-						<div class="sSeat"></div>
-					</td>
-					<td>
-						<div class="sSeat"></div>
-					</td>
-					<td>
-						<div class="sSeat"></div>
-					</td>
-					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssH05"></div>
 					</td>
 				</tr>
 				<tr>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssG01"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssG02"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssG03"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssG04"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssG05"></div>
 					</td>
 				</tr>
 				<tr>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssF01"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssF02"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssF03"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssF04"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssF05"></div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="sSeat" id="ssE01"></div>
+					</td>
+					<td>
+						<div class="sSeat" id="ssE02"></div>
+					</td>
+					<td>
+						<div class="sSeat" id="ssE03"></div>
+					</td>
+					<td>
+						<div class="sSeat" id="ssE04"></div>
+					</td>
+					<td>
+						<div class="sSeat" id="ssE05"></div>
 					</td>
 				</tr>
 <!--BeanBags-->
-				<tr class="seat4">
+				<tr>
+					<td></td>
+				</tr>
+				<tr>
 					<td></td>				
 					<td colspan="3">
-						<div class="bSeat"></div>
+						<div class="bSeat" id="bbD01">
+							<p class="bbText">D01</p>
+						</div>
 					</td>
 				</tr>
 				<tr>								
 					<td colspan="2">
-						<div class="bSeat"></div>
+						<div class="bSeat" id="bbC01" >
+							<p class="bbText">C01</p>
+						</div>
 					</td>
 					<td></td>
 					<td colspan="2">
-						<div class="bSeat"></div>
+						<div class="bSeat"  id="bbC02">
+							<p class="bbText">C02</p>
+						</div>
 					</td>
 				</tr>
 				<tr>
 					<td></td>				
 					<td colspan="3">
-						<div class="bSeat"></div>
+						<div class="bSeat"  id="bbB01">
+							<p class="bbText">B01</p>
+						</div>
 					</td>
 				</tr>
 			</table>
-			<table id:"seats2">
+			<table>
 				<tr>
 					<td>
-						<div class="fSeat"></div>
+						<div class="fSeat" id="fsG06"></div>
 					</td>
 					<td>
-						<div class="fSeat"></div>
+						<div class="fSeat" id="fsG07"></div>
 					</td>
 					<td>
 						
 					</td>
 					<td>
-						<div class="fSeat"></div>
+						<div class="fSeat" id="fsG08"></div>
 					</td>
 					<td>
-						<div class="fSeat"></div>
+						<div class="fSeat" id="fsG09"></div>
 					</td>
 				</tr>
 				<tr>
 					<td>
-						<div class="fSeat"></div>
+						<div class="fSeat" id="fsG06"></div>
 					</td>
 					<td>
-						<div class="fSeat"></div>
+						<div class="fSeat" id="fsG07"></div>
 					</td>
 					<td>
 						
 					</td>
 					<td>
-						<div class="fSeat"></div>
+						<div class="fSeat" id="fsG08"></div>
 					</td>
 					<td>
-						<div class="fSeat"></div>
+						<div class="fSeat" id="fsG09"></div>
 					</td>
 				</tr>
 				<tr>
 					<td>
-						<div class="fSeat"></div>
+						<div class="fSeat" id="fsG06"></div>
 					</td>
 					<td>
-						<div class="fSeat"></div>
+						<div class="fSeat" id="fsG07"></div>
 					</td>
 					<td>
 						
 					</td>
 					<td>
-						<div class="fSeat"></div>
+						<div class="fSeat" id="fsG08"></div>
 					</td>
 					<td>
-						<div class="fSeat"></div>
+						<div class="fSeat" id="fsG09"></div>
 					</td>
 				</tr>
 <!--BeanBags-->
+				<tr>
+					<td></td>
+				</tr>
 				<tr class="seat4">								
 					<td colspan="2">
-						<div class="bSeat"></div>
+						<div class="bSeat"id="bbD02">
+							<p class="bbText">D02</p>
+						</div>
 					</td>
 					<td></td>
 					<td colspan="2">
-						<div class="bSeat"></div>
+						<div class="bSeat"id="bbD03">
+							<p class="bbText">D03</p>
+						</div>
 					</td>
 				</tr>
 				<tr>
@@ -166,108 +185,125 @@
 				<tr>					
 					<td></td>
 					<td colspan="3">
-						<div class="bSeat"></div>
+						<div class="bSeat"id="bbB02">
+							<p class="bbText">B02</p>
+						</div>
 					</td>
 				</tr>
 				<tr>								
 					<td colspan="2">
-						<div class="bSeat"></div>
+						<div class="bSeat" id="bbA01">
+							<p class="bbText">A01</p>
+						</div>
 					</td>
 					<td></td>
 					<td colspan="2">
-						<div class="bSeat"></div>
+						<div class="bSeat"id="bbA02">
+							<p class="bbText">A02</p>
+						</div>
 					</td>
 				</tr>
 			</table>
 			<table id:"seats3">
 				<tr>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssH10"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssH11"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssH12"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssH13"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<div class="sSeat"></div>
-					</td>
-					<td>
-						<div class="sSeat"></div>
-					</td>
-					<td>
-						<div class="sSeat"></div>
-					</td>
-					<td>
-						<div class="sSeat"></div>
-					</td>
-					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssH14"></div>
 					</td>
 				</tr>
 				<tr>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssG10"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssG11"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssG12"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssG13"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssG14"></div>
 					</td>
 				</tr>
 				<tr>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssF10"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssF11"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssF12"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssF13"></div>
 					</td>
 					<td>
-						<div class="sSeat"></div>
+						<div class="sSeat" id="ssF14"></div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="sSeat" id="ssE10"></div>
+					</td>
+					<td>
+						<div class="sSeat" id="ssE11"></div>
+					</td>
+					<td>
+						<div class="sSeat" id="ssE12"></div>
+					</td>
+					<td>
+						<div class="sSeat" id="ssE13"></div>
+					</td>
+					<td>
+						<div class="sSeat" id="ssE14"></div>
 					</td>
 				</tr>
 <!--BeanBags-->
+				<tr>
+					<td></td>
+				</tr>
 				<tr class="seat4">
 					<td></td>				
 					<td colspan="3">
-						<div class="bSeat"></div>
+						<div class="bSeat" id="bbD04">
+							<p class="bbText">D04</p>
+						</div>
 					</td>
 				</tr>
 				<tr>								
 					<td colspan="2">
-						<div class="bSeat"></div>
+						<div class="bSeat" id="bbC03">
+							<p class="bbText">C03</p>
+						</div>
 					</td>
 					<td></td>
 					<td colspan="2">
-						<div class="bSeat"></div>
+						<div class="bSeat" id="bbC04">
+							<p class="bbText">C04</p>
+						</div>
 					</td>
 				</tr>
 				<tr>
 					<td></td>				
 					<td colspan="3">
-						<div class="bSeat"></div>
+						<div class="bSeat"id="bbB03">
+							<p class="bbText">B03</p>
+						</div>
 					</td>
 				</tr>
 			</table>			
