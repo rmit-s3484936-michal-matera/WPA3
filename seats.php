e386f51a035284f99a0b9a829c7883146a0d63f0
rmit-s3484936-michal-matera <s3484936@student.rmit.edu.au>
2015-05-13 14:37:14 +1000

    Seat rows added

--- seats.php
+++ seats.php
@@ -6,25 +6,197 @@
 	</head>
 	<body>
 		<div>
-			<table>
+			<table class:"seats1">
 				<tr>
-					<div class="sSeat">
-					</div>
-					<div class="sSeat">
-					</div>
-					<div class="sSeat">
-					</div>
-					<div class="sSeat">
-					</div>
-					<div class="sSeat">
-					</div>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
 				</tr>
 				<tr>
-					<td>vssdvssv</td>
-					<div class="sSeat">
-					</div>
-					<div class="sSeat">
-					</div>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+				</tr>
+			</table>
+			<table class:"seats2">
+				<tr>
+					<td>
+						<div class="fSeat"></div>
+					</td>
+					<td>
+						<div class="fSeat"></div>
+					</td>
+					<td>
+						
+					</td>
+					<td>
+						<div class="fSeat"></div>
+					</td>
+					<td>
+						<div class="fSeat"></div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="fSeat"></div>
+					</td>
+					<td>
+						<div class="fSeat"></div>
+					</td>
+					<td>
+						
+					</td>
+					<td>
+						<div class="fSeat"></div>
+					</td>
+					<td>
+						<div class="fSeat"></div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="fSeat"></div>
+					</td>
+					<td>
+						<div class="fSeat"></div>
+					</td>
+					<td>
+						
+					</td>
+					<td>
+						<div class="fSeat"></div>
+					</td>
+					<td>
+						<div class="fSeat"></div>
+					</td>
+				</tr>
+			</table>
+			<table class:"seats3">
+				<tr>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
+					<td>
+						<div class="sSeat"></div>
+					</td>
 				</tr>
 			</table>
 		</div>
