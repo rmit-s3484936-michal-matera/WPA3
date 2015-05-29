64a7cc8b24ecf25d4a7fea1c67ef2e9a894743d6
rmit-s3484936-michal-matera <s3484936@student.rmit.edu.au>
2015-05-29 12:30:44 +1000

    minor changes to customer

---
+++ css/index.php
@@ -0,0 +1,66 @@
+	<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="keywords" content="Cinema, Movie, Silverado">
+		<meta name="description" content="Silverado Cinema website">		
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/style-index.css">
+	</head>
+	<title>Silverado Cinema Home</title>
+		<?php include 'includes/header.php';?>
+		
+		
+		
+			<div id="main">
+				<div id="text">
+					<h2>Bookings</h2>
+					Want to buy your tickets from home?
+					<br>
+					Then no problem!
+					<br>
+					You can buy tickets for any of our movies on whichever date they air by simply clicking on a day under the movie tabs.
+					<hr>
+					<h2>New Movies</h2>
+					Today we have pleasure to inform you that we are now airing our first four movies which you may have seen on the right side, where we will now be showing the top movies of the week.
+					<hr>
+					<h2>Silverado version 2 is up!</h2>
+					We have completely redesigned the website after feedback from our users. We hope that you will enjoy our new website.
+					<hr>				
+					<h2>First Version of Silverado</h2>
+					Here is what has been implemented so far:
+						<ul>
+							<li>Home Page</li>
+							<li>Contact Page</li>
+						</ul>
+					<hr>
+					<h2>Greetings to everyone</h2>	
+					Welcome to Silverado Cinema!
+					<br>
+					We are a small Cinema which has decided to embrace technology and create our own website. We hope that this will be a benefit to everyone and that you will all enjoy it.
+					<hr>
+
+				</div>
+				<div id="movietabshowcase">
+					Movies Airing		
+					<hr class="tab">	
+					<a href="Movies.html">		
+					</a>
+						<img class="hmImage" src="images/FindingNemo3D.jpg" alt="Finding Nemo 3D" >
+					<hr class="tab">	
+					<a href="Movies.html">	
+						<img class="hmImage" src="images/BatmanBegins.jpg" alt="Batman Begins">
+					</a>
+					<hr class="tab">
+					<a href="Movies.html">		
+						<img class="hmImage" src="images/Akira.jpg" alt="Akira">
+					</a>
+					<hr class="tab">	
+					<a href="Movies.html">	
+						<img class="hmImage" src="images/27Dresses.jpg" alt="27 Dresses">
+					</a>
+				</div>
+			</div>	
+		<?php include 'includes/footer.php';?>
+	</body>
+</html>
+			
--- customerd.php
+++ customerd.php
@@ -3,12 +3,14 @@
 	<head>
 		<link rel="stylesheet" href="css/style-ticket.css">
 		<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/style-index.css">
 		<title>Customer Details</title>
 	</head>
 	<body>
+		<?php include 'includes/header.php';?>
 		<div id="bookPg">
 			<div id="cusDet">
-				<form>
+				<form method="post">
 					Name
 					<input type="text" placeholder="John Smith" pattern="^[A-z][a-z]{1,15}[ ][A-Z][a-z]{1,25}$" required>		
 					<br>
@@ -18,7 +20,7 @@
 					Phone
 					<input type="tel" pattern="^(\(04\)|04|\+614)[ ]?\d{4}[ ]?\d{4}$" placeholder="04/(04)/+614 4444 4444" required>
 					<br><br>
-					<input type="submit">
+					<input type="submit" onclick="function echoPost()">
 					<input type="reset">
 
 				</form>
@@ -27,6 +29,17 @@
 
 			</div>			
 		</div>
+		<?php include 'includes/footer.php';?>
+<?php
+ function echoPost(){
+foreach( $_POST as $key => $value ){
+    echo $key."\t=>\t".$value."\n";}
+
+}
+
+?>
+
+
 	</body>
 </html>
 
