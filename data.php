1d09e0e7b43128d336e338a508dbd770abe1a428
saj812 <saj812@gmail.com>
2015-06-03 13:06:30 +1000

    json
    fixed json data file format

--- data.php
+++ data.php
@@ -1,66 +1,112 @@
 
 {
-  "AF": {
-    "title": "Akira",
-    "poster": "images/Akira.jpg",
-    "rating": "Rated R",
-	"description": "A secret military project endangers Neo-Tokyo when it turns a biker gang member into a rampaging psionic psychopath that only two kids and a group of psionics can stop.",
-	"dir": "Director: Katsuhiro Ôtomo",
-	"cast": "Cast: Nozomu Sasaki, Mami Koyama, Mitsuo Iwata",
-    "sessions": {
-      "Monday": "6pm",
-      "Tuesday": "6pm",
-      "Saturday": "3pm",
-      "Sunday": "3pm"
+    "AF": {
+        "title": "Akira",
+        "poster": "images/Akira.jpg",
+        "rating": "Rated R",
+        "description": "A secret military project endangers Neo-Tokyo when it turns a biker gang member into a rampaging psionic psychopath that only two kids and a group of psionics can stop.",
+        "dir": "Director: Katsuhiro Ôtomo",
+        "cast": "Cast: Nozomu Sasaki, Mami Koyama, Mitsuo Iwata",
+        "sessions": [
+            {
+                "Monday": "6pm"
+            },
+            {
+                "Tuesday": "6pm"
+            },
+            {
+                "Saturday": "3pm"
+            },
+            {
+                "Sunday": "3pm"
+            }
+        ]
+    },
+    "CH": {
+        "title": "Finding Nemo 3D",
+        "poster": "images/FindingNemo3D.jpg",
+        "rating": "Rated PG",
+        "description": "After his son is captured in the Great Barrier Reef and taken to Sydney, a timid clownfish sets out on a journey to bring him home.",
+        "dir": "Directors: Andrew Stanton, Lee Unkrich",
+        "cast": "Cast: Albert Brooks, Ellen DeGeneres, Alexander Gould",
+        "sessions": [
+            {
+                "Monday": "1pm"
+            },
+            {
+                "Tuesday": "1pm"
+            },
+            {
+                "Wednesday": "6pm"
+            },
+            {
+                "Thursday": "6pm"
+            },
+            {
+                "Friday": "6pm"
+            },
+            {
+                "Saturday": "12pm"
+            },
+            {
+                "Sunday": "12pm"
+            }
+        ]
+    },
+    "RC": {
+        "title": "27 Dresses",
+        "poster": "images/27Dresses.jpg",
+        "rating": "Rated PG",
+        "description": "After serving as a bridesmaid 27 times, a young woman wrestles with the idea of standing by her sister's side as her sibling marries the man she's secretly in love with.",
+        "dir": "Director: Anne Fletcher",
+        "cast": "Cast: Katherine Heigl, James Marsden, Malin Akerman",
+        "sessions": [
+            {
+                "Monday": "9pm"
+            },
+            {
+                "Tuesday": "9pm"
+            },
+            {
+                "Wednesday": "1pm"
+            },
+            {
+                "Thursday": "1pm"
+            },
+            {
+                "Friday": "1pm"
+            },
+            {
+                "Saturday": "6pm"
+            },
+            {
+                "Sunday": "6pm"
+            }
+        ]
+    },
+    "AC": {
+        "title": "Batman Begins",
+        "poster": "images/BatmanBegins.jpg",
+        "rating": "Rated M",
+        "description": "After training with his mentor, Batman begins his war on crime to free the crime-ridden Gotham City from corruption that the Scarecrow and the League of Shadows have cast upon it.",
+        "dir": "Director: Christopher Nolan",
+        "cast": "Cast: Christian Bale, Michael Caine, Ken Watanabe",
+        "sessions": [
+            {
+                "Wednesday": "9pm"
+            },
+            {
+                "Thursday": "9pm"
+            },
+            {
+                "Friday": "9pm"
+            },
+            {
+                "Saturday": "9pm"
+            },
+            {
+                "Sunday": "9pm"
+            }
+        ]
     }
-  },
-  "CH": {
-    "title": "Finding Nemo 3D",
-    "poster": "images/FindingNemo3D.jpg",
-    "rating": "Rated PG",
-	"description":"After his son is captured in the Great Barrier Reef and taken to Sydney, a timid clownfish sets out on a journey to bring him home.",
-	"dir":"Directors: Andrew Stanton, Lee Unkrich",
-	"cast":"Cast: Albert Brooks, Ellen DeGeneres, Alexander Gould",
-    "sessions": {
-      "Monday": "1pm",
-      "Tuesday": "1pm",
-      "Wednesday": "6pm",
-      "Thursday": "6pm",
-      "Friday": "6pm",
-      "Saturday": "12pm",
-      "Sunday": "12pm"
-    }
-  },
-  "RC": {
-    "title": "27 Dresses",
-    "poster": "images/27Dresses.jpg",
-    "rating": "Rated PG",
-	"description":"After serving as a bridesmaid 27 times, a young woman wrestles with the idea of standing by her sister's side as her sibling marries the man she's secretly in love with.",
-	"dir":"Director: Anne Fletcher",
-    "cast":"Cast: Katherine Heigl, James Marsden, Malin Akerman",
-    "sessions": {
-      "Monday": "9pm",
-      "Tuesday": "9pm",
-      "Wednesday": "1pm",
-      "Thursday": "1pm",
-      "Friday": "1pm",
-      "Saturday": "6pm",
-      "Sunday": "6pm"
-    }
-  },
-  "AC": {
-    "title": "Batman Begins",
-    "poster": "images/BatmanBegins.jpg",
-    "rating": "Rated M",
-	"description":"After training with his mentor, Batman begins his war on crime to free the crime-ridden Gotham City from corruption that the Scarecrow and the League of Shadows have cast upon it.",
-	"dir":"Director: Christopher Nolan",
-	"cast":"Cast: Christian Bale, Michael Caine, Ken Watanabe",
-    "sessions": {
-      "Wednesday": "9pm",
-      "Thursday": "9pm",
-      "Friday": "9pm",
-      "Saturday": "9pm",
-      "Sunday": "9pm"
-    }
-  }
 }
